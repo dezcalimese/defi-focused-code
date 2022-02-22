f38d5e0262e76287df8ae114a3f7a78339bf5e8f
Dez Calimese <dezcalimese@gmail.com>
2022-02-22 17:34:22 -0500

    payment splitter contract

--- flashloans/contracts/IFlashloanUser.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.7.3;
-
-interface IFLashloanUser {
-   function flashloanCallback(uint amount, address token, bytes memory data) external;
-}
--- oracles/contracts/IOracle.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.7.3;
-
-interface IOracle {
-   function getData(bytes32 key)
-      external
-      view
-      returns(bool result, uint date, uint payload);
-}
